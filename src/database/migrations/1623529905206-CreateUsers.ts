a14afb9ec8c69fc6c95fd82c8877301ecb390e26
Alessandro Lins <aflinsjr@icloud.com>
2021-06-13 08:35:17 -0300

    preparando ambiente login

---
+++ src/database/migrations/1623529905206-CreateUsers.ts
@@ -0,0 +1,48 @@
+import {MigrationInterface, QueryRunner, Table } from "typeorm";
+import { uuid } from 'uuidv4';
+
+export class CreateUsers1623529905206 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+        
+        await queryRunner.createTable(new Table({
+            name: "users",
+            columns: [
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true,
+                    generationStrategy: "uuid",
+                    default: 'uuid_generate_v4()'
+                },
+                    
+                {
+                    name: "name",
+                    type: "varchar",
+                },
+                    
+                {
+                    name: "username",
+                    type: "varchar",
+                },
+                    
+                {
+                    name: "password",
+                    type: "varchar",
+                },
+                
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: 'now()'
+                },
+            ],
+        }));
+    }
+        
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+        await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+    }
+}
+    
